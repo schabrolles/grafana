cc08279cf173e10acdf75e132759fc85557ba9ca
bergquist <carl.bergquist@gmail.com>
2017-01-30 17:27:12 +0100

    tech(build): flip if statement....

--- scripts/build/build.sh
+++ scripts/build/build.sh
@@ -10,15 +10,15 @@ REPO_PATH=$GOPATH/src/github.com/grafana/grafana
 mkdir -p /go/src/github.com/grafana
 cd /go/src/github.com/grafana
 
-if [ -n "${CIRCLE_TAG}" ]; then
-  echo "Building from tag ${CIRCLE_TAG}"
-  git clone --depth 1 https://github.com/grafana/grafana.git -b $CIRCLE_BRANCH
-  cd $REPO_PATH
-else
-  echo "Building from branch ${CIRCLE_BRANCH}"
+if [ -z "$CIRCLE_TAG" ]; then
+  echo "Building from branch $CIRCLE_BRANCH"
   git clone https://github.com/grafana/grafana.git
   cd $REPO_PATH
   git checkout $CIRCLE_TAG
+else
+  echo "Builing from tag $CIRCLE_TAG"
+  git clone --depth 1 https://github.com/grafana/grafana.git -b $CIRCLE_BRANCH
+  cd $REPO_PATH
 fi
 
 go run build.go build
@@ -29,11 +29,11 @@ rvm use 2.1.9 --default
 
 gem install fpm -v 1.4
 
-#if [ -n "${CIRCLE_TAG}" ]; then
-  #go run build.go -includeBuildNumber=false package latest
-#else
-go run build.go package latest
-#fi
+if [ -z "$CIRCLE_TAG" ]; then
+  go run build.go -includeBuildNumber=false package latest
+else
+  go run build.go package latest
+fi
 
 cp dist/* /tmp/dist/
 
